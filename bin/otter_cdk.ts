2b779bc5fd05b3e61d7673146bc9234fcf2f5f12
Kayo <kayo@kayo.moe>
2021-02-27 20:01:50 -0800

    Add pipeline

--- bin/otter_cdk.ts
+++ bin/otter_cdk.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { OtterCdkStack } from '../lib/otter_cdk-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { OtterBotStack, OtterCdkPipelineStack } from "../lib/otter_cdk-stack";
 
 const app = new cdk.App();
-new OtterCdkStack(app, 'OtterCdkStack');
+
+const pipelineStack = new OtterCdkPipelineStack(app, "OtterBotPipelineStack");
+new OtterBotStack(app, "OtterBotStack", {
+  image: pipelineStack.tagParameterContainerImage,
+});
+
+app.synth();
--- lib/otter_cdk-stack.ts
+++ lib/otter_cdk-stack.ts
@@ -1,45 +1,239 @@
-import { config } from "dotenv"; config();
-import { Stack, Construct, StackProps, Duration } from '@aws-cdk/core';
-import { Vpc } from "@aws-cdk/aws-ec2";
-import { Cluster, ContainerImage } from "@aws-cdk/aws-ecs";
-import { NetworkLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns";
-import { Secret } from "@aws-cdk/aws-secretsmanager";
+import { config } from "dotenv";
+config();
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as ssm from "@aws-cdk/aws-secretsmanager";
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codebuild from "@aws-cdk/aws-codebuild";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import * as ecr from "@aws-cdk/aws-ecr";
+import { TagParameterContainerImage } from "@aws-cdk/aws-ecs";
 
-export class OtterCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export interface OtterBotStackProps extends cdk.StackProps {
+  readonly image: ecs.ContainerImage;
+}
+
+export class OtterBotStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: OtterBotStackProps) {
     super(scope, id, props);
 
-    if (!process.env.OTTER_BOT_TOKEN_SECRET_ARN) throw new Error("OTTER_BOT_TOKEN_SECRET_ARN must be defined!");
+    if (!process.env.OTTER_BOT_TOKEN_SECRET_ARN)
+      throw new Error("OTTER_BOT_TOKEN_SECRET_ARN must be defined!");
 
-    const vpc = new Vpc(this, 'OtterVPC', { maxAzs: 2 });
-    const cluster = new Cluster(this, 'OtterFargate', { vpc });
+    const vpc = new ec2.Vpc(this, "OtterVPC", { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, "OtterFargate", { vpc });
 
-    const discordBotTokenSecret = Secret.fromSecretAttributes(this, "OtterBotTokenSecret", {
-      secretCompleteArn: process.env.OTTER_BOT_TOKEN_SECRET_ARN
-    });
-
-    const fargateService = new NetworkLoadBalancedFargateService(this, "OtterApp", {
-      cluster,
-      taskImageOptions: {
-        image: ContainerImage.fromRegistry("kayochuu/otter"),
-        environment: {
-          "DISCORD_BOT_TOKEN": discordBotTokenSecret.secretValue.toString()
-        }
+    const discordBotTokenSecret = ssm.Secret.fromSecretAttributes(
+      this,
+      "OtterBotTokenSecret",
+      {
+        secretCompleteArn: process.env.OTTER_BOT_TOKEN_SECRET_ARN,
       }
+    );
+
+    const fargateService = new ecs_patterns.NetworkLoadBalancedFargateService(
+      this,
+      "OtterApp",
+      {
+        cluster,
+        taskImageOptions: {
+          image: props.image,
+          environment: {
+            DISCORD_BOT_TOKEN: discordBotTokenSecret.secretValue.toString(),
+          },
+        },
+      }
+    );
+
+    const scaling = fargateService.service.autoScaleTaskCount({
+      maxCapacity: 2,
     });
 
-    const scaling = fargateService.service.autoScaleTaskCount({ maxCapacity: 2 });
-
-    scaling.scaleOnMemoryUtilization('MemScaling', {
+    scaling.scaleOnMemoryUtilization("MemScaling", {
       targetUtilizationPercent: 90,
-      scaleInCooldown: Duration.minutes(30),
-      scaleOutCooldown: Duration.minutes(30)
+      scaleInCooldown: cdk.Duration.minutes(30),
+      scaleOutCooldown: cdk.Duration.minutes(30),
     });
 
-    scaling.scaleOnCpuUtilization('CpuScaling', {
+    scaling.scaleOnCpuUtilization("CpuScaling", {
       targetUtilizationPercent: 90,
-      scaleInCooldown: Duration.minutes(30),
-      scaleOutCooldown: Duration.minutes(30)
+      scaleInCooldown: cdk.Duration.minutes(30),
+      scaleOutCooldown: cdk.Duration.minutes(30),
+    });
+  }
+}
+
+/**
+ * Most of this was taken from:
+ *  https://github.com/aws/aws-cdk/blob/master/packages/%40aws-cdk/aws-codepipeline-actions/test/integ.pipeline-ecs-separate-source.lit.ts
+ */
+export class OtterCdkPipelineStack extends cdk.Stack {
+  public readonly tagParameterContainerImage: TagParameterContainerImage;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /* ********** ECS part **************** */
+
+    // this is the ECR repository where the built Docker image will be pushed
+    const appEcrRepo = new ecr.Repository(this, "OtterBotRepo");
+    // the build that creates the Docker image, and pushes it to the ECR repo
+    const appCodeDockerBuild = new codebuild.PipelineProject(
+      this,
+      "OtterBotCodeDockerImageBuildAndPushProject",
+      {
+        environment: {
+          // we need to run Docker
+          privileged: true,
+        },
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            build: {
+              commands: [
+                // login to ECR first
+                "$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)",
+                // if your application needs any build steps, they would be invoked here
+
+                // build the image, and tag it with the commit hash
+                // (CODEBUILD_RESOLVED_SOURCE_VERSION is a special environment variable available in CodeBuild)
+                "docker build -t $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION .",
+              ],
+            },
+            post_build: {
+              commands: [
+                // push the built image into the ECR repository
+                "docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                // save the declared tag as an environment variable,
+                // that is then exported below in the 'exported-variables' section as a CodePipeline Variable
+                "export imageTag=$CODEBUILD_RESOLVED_SOURCE_VERSION",
+              ],
+            },
+          },
+          env: {
+            // save the imageTag environment variable as a CodePipeline Variable
+            "exported-variables": ["imageTag"],
+          },
+        }),
+        environmentVariables: {
+          REPOSITORY_URI: {
+            value: appEcrRepo.repositoryUri,
+          },
+        },
+      }
+    );
+    // needed for `docker push`
+    appEcrRepo.grantPullPush(appCodeDockerBuild);
+    // create the ContainerImage used for the ECS application Stack
+    this.tagParameterContainerImage = new ecs.TagParameterContainerImage(
+      appEcrRepo
+    );
+
+    const cdkCodeBuild = new codebuild.PipelineProject(
+      this,
+      "OtterCdkCodeBuildProject",
+      {
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            install: {
+              commands: ["npm install"],
+            },
+            build: {
+              commands: [
+                // synthesize the CDK code for the ECS application Stack
+                "npx cdk synth --verbose",
+              ],
+            },
+          },
+          artifacts: {
+            // store the entire Cloud Assembly as the output artifact
+            "base-directory": "cdk.out",
+            files: "**/*",
+          },
+        }),
+      }
+    );
+
+    /* ********** Pipeline part **************** */
+
+    const appCodeSourceOutput = new codepipeline.Artifact();
+    const cdkCodeSourceOutput = new codepipeline.Artifact();
+    const cdkCodeBuildOutput = new codepipeline.Artifact();
+    const appCodeBuildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: "OtterBotCodeDockerImageBuildAndPush",
+      project: appCodeDockerBuild,
+      input: appCodeSourceOutput,
+    });
+    new codepipeline.Pipeline(this, "CodePipelineDeployingEcsApplication", {
+      artifactBucket: new s3.Bucket(this, "ArtifactBucket", {
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      }),
+      stages: [
+        {
+          stageName: "Source",
+          actions: [
+            // this is the Action that takes the source of your application code
+            new codepipeline_actions.GitHubSourceAction({
+              actionName: "OtterBotCodeSource",
+              owner: "RyoshiKayo",
+              repo: "Otter",
+              output: appCodeSourceOutput,
+              branch: "main",
+              oauthToken: cdk.SecretValue.secretsManager(
+                "RyoshiKayo-github-token"
+              ),
+            }),
+            // this is the Action that takes the source of your CDK code
+            // (which would probably include this Pipeline code as well)
+            new codepipeline_actions.GitHubSourceAction({
+              actionName: "OtterCdkCodeSource",
+              owner: "RyoshiKayo",
+              repo: "OtterCDK",
+              output: cdkCodeSourceOutput,
+              branch: "main",
+              oauthToken: cdk.SecretValue.secretsManager(
+                "RyoshiKayo-github-token"
+              ),
+            }),
+          ],
+        },
+        {
+          stageName: "Build",
+          actions: [
+            appCodeBuildAction,
+            new codepipeline_actions.CodeBuildAction({
+              actionName: "OtterCdkCodeBuildAndSynth",
+              project: cdkCodeBuild,
+              input: cdkCodeSourceOutput,
+              outputs: [cdkCodeBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: "Deploy",
+          actions: [
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: "CFN_Deploy",
+              stackName: "SampleEcsStackDeployedFromCodePipeline",
+              // this name has to be the same name as used below in the CDK code for the application Stack
+              templatePath: cdkCodeBuildOutput.atPath(
+                "EcsStackDeployedInPipeline.template.json"
+              ),
+              adminPermissions: true,
+              parameterOverrides: {
+                // read the tag pushed to the ECR repository from the CodePipeline Variable saved by the application build step,
+                // and pass it as the CloudFormation Parameter for the tag
+                [this.tagParameterContainerImage
+                  .tagParameterName]: appCodeBuildAction.variable("imageTag"),
+              },
+            }),
+          ],
+        },
+      ],
     });
   }
 }
