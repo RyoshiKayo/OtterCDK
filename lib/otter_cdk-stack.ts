b7738572f469e609ee7a6624fd702c0364e34bcf
Kayo <kayo@kayo.moe>
2021-03-01 18:20:03 -0800

    fix(Image): Set Prefix and Owner env variables

--- lib/otter_cdk-stack.ts
+++ lib/otter_cdk-stack.ts
@@ -38,6 +38,8 @@ export class OtterBotStack extends cdk.Stack {
         taskImageOptions: {
           image: props.image,
           environment: {
+            DISCORD_BOT_PREFIX: "o!",
+            DISCORD_BOT_OWNER: '132266422679240704',
             DISCORD_BOT_TOKEN: discordBotTokenSecret.secretValue.toString(),
           },
         },
